48204b00004ac4d553d5d21d691fb9c27c743e34
rexchoppers <connorbrady@codox.uk>
2025-01-16 21:01:11 +0000

    WIP Starting on command line seeders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/commands/command.module.ts
@@ -0,0 +1,6 @@
+import { Module } from "@nestjs/common";
+
+@Module({
+  imports: [CommandModule],
+})
+export class CommandModule {}
