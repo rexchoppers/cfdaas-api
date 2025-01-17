91fe7b9334352317465f33d39cb7ea1753c60b90
rexchoppers <connorbrady@codox.uk>
2025-01-17 08:59:11 +0000

    WIP User service information

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from '../entities/user.entity';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
 
+  async createUser(data: {
+    email: string;
+    firstName: string;
+    lastName: string;
+  }) {
+    
+
+  }
 }
