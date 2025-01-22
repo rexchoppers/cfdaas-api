310746a9c793ed98a60201aa44ebc212f9c8a580
rexchoppers <connorbrady@codox.uk>
2025-01-22 12:48:13 +0000

    ADDED Access service

---
+++ src/services/access.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Access } from '../entities/access.entity';
+
+@Injectable()
+export class AccessService {
+  constructor(
+    @InjectModel(Access.name) private readonly accessModel: Model<Access>,
+  ) {}
+
+  async getAccess(userId: string, companyId: string) {
+    return this.accessModel.findOne({
+      user: userId,
+      company: companyId,
+    });
+  }
+
+  async addAccess(userId: string, companyId: string) {
+    const access = new this.accessModel({
+      user: userId,
+      company: companyId,
+    });
+
+    return await access.save();
+  }
+}
