7b5619072e4dfe87c6f1d37e424a25fdb73d8283
rexchoppers <connorbrady@codox.uk>
2025-02-01 20:00:15 +0000

    ADDED Attributes in GetUserResponse

--- src/commands/responses/get-user.response.ts
+++ src/commands/responses/get-user.response.ts
@@ -1,6 +1,24 @@
-import { Expose, Transform } from 'class-transformer';
+import { Expose } from 'class-transformer';
 
 export class GetUserResponse {
   @Expose()
   id: string;
+
+  @Expose()
+  email: string;
+
+  @Expose()
+  firstName: string;
+
+  @Expose()
+  lastName: string;
+
+  @Expose()
+  active: boolean;
+
+  @Expose()
+  createdAt: Date;
+
+  @Expose()
+  updatedAt: Date;
 }
