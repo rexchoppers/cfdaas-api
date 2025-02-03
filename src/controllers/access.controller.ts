be9489546750725fd7af646d5d9206fa68830d83
rexchoppers <connorbrady@codox.uk>
2025-02-03 10:23:30 +0000

    UPDATED Access controller

--- src/controllers/access.controller.ts
+++ src/controllers/access.controller.ts
@@ -4,21 +4,28 @@ import { CognitoJwtPayload } from 'aws-jwt-verify/jwt-model';
 import { UserService } from '../services/user.service';
 import { plainToInstance } from 'class-transformer';
 import { GetUserResponse } from '../responses/get-user.response';
+import { AccessService } from '../services/access.service';
+import { AccessResponse } from "../responses/access.response";
 
 @Controller('access')
 @Authentication()
 export class AccessController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly accessService: AccessService,
+  ) {}
 
   @Get()
   async getAccesses(
     @CognitoUser() cognitoUser: CognitoJwtPayload,
-  ): Promise<GetUserResponse> {
+  ): Promise<AccessResponse[]> {
     const sub = cognitoUser.sub;
 
     const user = await this.userService.getUser({ cognitoId: sub });
 
-    return plainToInstance(GetUserResponse, user, {
+    const accesses = await this.accessService.getAccesses(user.id);
+
+    return plainToInstance(AccessResponse, accesses, {
       excludeExtraneousValues: true,
     });
   }
