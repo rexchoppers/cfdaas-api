d28c9bf8c045c01d7ac30f69f1d70d0f7b027f58
rexchoppers <connorbrady@codox.uk>
2025-02-04 21:21:25 +0000

    WIP Adding create profile request

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -1,11 +1,20 @@
 import { Authentication, CognitoUser } from '@nestjs-cognito/auth';
-import { Controller, ForbiddenException, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  ForbiddenException,
+  Get,
+  Param,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UserService } from '../services/user.service';
 import { CompanyService } from '../services/company.service';
 import { AccessService } from 'src/services/access.service';
 import { CognitoJwtPayload } from 'aws-jwt-verify/jwt-model';
+import { CreateProfileRequest } from '../requests/create-profile.request';
 
-@Controller('profile')
+@Controller()
 @Authentication()
 export class ProfileController {
   constructor(
@@ -14,7 +23,7 @@ export class ProfileController {
     private readonly accessService: AccessService,
   ) {}
 
-  @Get('/:companyId/profile')
+  @Get('/company/:companyId/profile')
   async getProfiles(
     @CognitoUser() cognitoUser: CognitoJwtPayload,
     @Param('companyId') companyId: string,
@@ -31,5 +40,29 @@ export class ProfileController {
     if (!access) {
       throw new ForbiddenException();
     }
+  }
+
+  @Post('/company/:companyId/profile')
+  async createProfile(
+    @CognitoUser() cognitoUser: CognitoJwtPayload,
+    @Param('companyId') companyId: string,
+    @Body(ValidationPipe) createProfileRequest: CreateProfileRequest,
+  ) {
+    const user = await this.userService.getUser({ cognitoId: cognitoUser.sub });
+    const company = await this.companyService.getCompany(companyId);
+
+    if (!company) {
+      throw new ForbiddenException();
+    }
+
+    const access = await this.accessService.getAccess(user.id, company.id);
+
+    if (!access) {
+      throw new ForbiddenException();
+    }
+
+    console.log("Creating profile...");
+
+    return 'test';
   }
 }
