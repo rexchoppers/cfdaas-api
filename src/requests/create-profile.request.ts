e06e97d950ff91bdb17d0e11091530769df2cffd
rexchoppers <connorbrady@codox.uk>
2025-02-06 09:38:20 +0000

    WIP Main validation request

--- src/requests/create-profile.request.ts
+++ src/requests/create-profile.request.ts
@@ -1,15 +1,25 @@
-import { IsBase64, IsEnum, IsNotEmpty } from 'class-validator';
+import { IsBase64, IsEnum, IsNotEmpty, ValidateIf } from 'class-validator';
 
 export enum Platform {
   AWS = 'AWS',
   GCP = 'GCP',
 }
 
+export enum GCPCredentialType {
+  JSON_SERVICE_ACCOUNT = 'json-service-account',
+}
+
 export class CreateProfileRequest {
   @IsEnum(Platform)
   platform: Platform;
 
-  @IsNotEmpty()
+  @ValidateIf((o) => o.platform === Platform.GCP)
+  @IsEnum(GCPCredentialType, {
+    message: 'For GCP, credentialType must be: json-service-account',
+  })
+  credentialType: GCPCredentialType;
+
+  @IsNotEmpty({ message: 'Credential data is required' })
   @IsBase64()
-  data: string;
+  credentialData: string;
 }
