71e43db07d69051bfcc0d2a5ffdfd642f0345814
rexchoppers <connorbrady@codox.uk>
2025-02-28 08:35:26 +0000

    ADDED Function to handle permissions

--- src/services/access.service.ts
+++ src/services/access.service.ts
@@ -9,6 +9,54 @@ export class AccessService {
     @InjectModel(Access.name) private readonly accessModel: Model<Access>,
   ) {}
 
+  async canPerformAction(
+    userId: string,
+    companyId: string,
+    resource: string,
+    action: 'view' | 'edit' | 'delete',
+  ) {
+    const ownerOnly = {
+      company: 'delete',
+    };
+
+    const adminOnly = {
+      team: 'edit',
+    };
+
+    const access = await this.getAccess(userId, companyId);
+
+    // If the access has not been found, don't allow the action
+    if (!access) {
+      return false;
+    }
+
+    // Owners can do anything
+    if (access.level === 'owner') {
+      return true;
+    }
+
+    // Admins can do anything except what's defined in the ownerOnly object
+    if (access.level === 'admin' && !ownerOnly[resource]) {
+      return true;
+    }
+
+    // Editors can only do anything that isn't in the adminOnly or ownerOnly objects
+    if (
+      access.level === 'editor' &&
+      !adminOnly[resource] &&
+      !ownerOnly[resource]
+    ) {
+      return true;
+    }
+
+    // Viewers can only view
+    if (access.level === 'viewer' && action === 'view') {
+      return true;
+    }
+
+    return false;
+  }
+
   async getAccess(userId: string, companyId: string) {
     return this.accessModel.findOne({
       user: userId,
